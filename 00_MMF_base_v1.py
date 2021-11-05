02d349cda8ed92aad5b54fdb99455c0fa66bf9aa
P-Teise <teumoh.teise@awatapu.school.nz>
2021-11-05 14:10:04 +1300

    base component made, comments added

---
+++ 00_MMF_base_v1.py
@@ -0,0 +1,21 @@
+# Functions go here
+# check that shape name is not blank
+
+
+# checks for an integer more than 0
+
+
+# ******* Main routine ***********
+
+# <<<<<<< Loop to get shape detail >>>>>>>>
+# start of loop
+# initialise loop so that it runs at least once
+
+
+
+
+
+
+# Get name (can't be blank)
+
+# End of shape loop
