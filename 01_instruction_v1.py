0d40236e5f04d634513b9a10628beef574d82ecc
P-Teise <teumoh.teise@awatapu.school.nz>
2021-11-08 09:35:51 +1300

    instruction component functional. not_blank function made and works

---
+++ 01_instruction_v1.py
@@ -0,0 +1,4 @@
+# functions go here
+
+instruction = "Hi! You can calculate the area of shapes with up to 5 sides"
+print(instruction)
---
+++ 02_not_blank_v1.py
@@ -0,0 +1,16 @@
+# functions go here
+
+
+def not_blank(question):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        if response != "":
+            return response
+        else:
+            print("Sorry - this can't be blank")
+
+
+name = not_blank("Shape name: ")
