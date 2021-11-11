72a244122e0a995b2ebcd108c76626c0adc6d27a
P-Teise <teumoh.teise@awatapu.school.nz>
2021-11-11 13:15:07 +1300

    found simpler way to do units -> instruction created

--- 04_unit_checker_v1.py
+++ 04_unit_checker_v1.py
@@ -11,20 +11,22 @@ def units(question):
 
         if response == "mm" or response == "millimetres":
             return "mm"
-        elif response == "cm" or response == "centimetres":
+        elif response == "cm" or response == "centimeters":
             return "cm"
-        elif response == "m" or response == "metres":
+        elif response == "m" or response == "meters":
             return "m"
         elif response == "in" or response == "inches":
             return "in"
         elif response == "km" or response == "kilometres":
             return "km"
+        elif response == "feet" or response == "foot" or response == "ft":
+            return "feet"
         else:
             print(error)
 
 
 # main routine goes here
 
-wanted_area = ("What shape area do you want to find?")
+wanted_area = "What shape area do you want to find?"
 print("Answer OK, you said", wanted_area)
 print()
---
+++ 05_unit_instruction_v1.py
@@ -0,0 +1,4 @@
+# functions go here 
+
+units_instruction: str = "Please enter the lengths of your desired shape in metres"
+print(units_instruction)
