0ac08cbff5b8022a2dbabe5972049fe28f20e127
P-Teise <teumoh.teise@awatapu.school.nz>
2021-11-17 10:25:12 +1300

    used .isnumeric() func to check numbers. code is not returning- not work

--- 06_number_checker_v1.py
+++ 06_number_checker_v1.py
@@ -1,19 +1,18 @@
 # functions go here
 
-def int_check(question, low_num, high_num):
-
-    error = "Please enter a valid number higher than {}". format(low_num)
+def int_check(question, low_num) -> object:
+    error = "Please enter a valid number higher than {}".format(low_num)
 
     valid = False
     while not valid:
 
         # ask user for number and check it is valid
         try:
-            response = int(input(question))
+            value_1 = int(input(question))
 
-            if low_num <= response:
-                return response
-            if response = str(input())
+            if low_num <= value_1:
+                print(value_1.isnumeric())
+
             else:
                 print(error)
 
@@ -21,7 +20,11 @@ def int_check(question, low_num, high_num):
         except ValueError:
             print(error)
 
+
 # main routine goes here
 
-
-length = int_check("Length 1: ", 0, 130)
+length = int_check("Enter Length 1: ", 0)
+print("Answer, OK you said:", length)
+print()
+#     if  = value_1:
+#                 return error return value_1
